b75837f6423cac5b44bc5dc583923cc60ed3e78a
Miguel Angel Amador Nava <miguel.amador.n@gmail.com>
2026-03-22 21:13:27 -0600

    add missing magazines

--- assets/app.js
+++ assets/app.js
@@ -12,7 +12,7 @@ const revistas = [
     cover: "../assets/cover/LAS DOCTRINAS BIBLICAS DE LA SALVACION 2P.gif",
   },
   {
-    title: "¿Qué es el discipulado?",
+    title: "¿Qué es el Discipulado?",
     description: "Fundamentos del discipulado cristiano para nuevos creyentes.",
     href: "./revistas/que-es-el-discipulado/index.html",
     cover: "../assets/cover/¿QUÉ ES EL DISCÍPULADO.jpg",
@@ -46,6 +46,198 @@ const revistas = [
      description: "Versión preliminar.",
      href: "./revistas/el-precio-de-ser-cristiano-en-tiempos-modernos/index.html",
      cover: "../assets/cover/El Precio De Ser Cristiano En Tiempos Modernos.png",
+   },
+     {
+       "title": "1999 Dios te Necesita Ahora",
+       "description": "Versión preliminar.",
+       "href": "./revistas/1999-Dios-te-necesita-ahora/index.html",
+       "cover": "../assets/cover/1999 DIOS TE NECESITA AHORA.gif"
+     },
+     {
+       "title": "Arrepentíos porque el Reino de los Cielos se ha Acercado",
+       "description": "Versión preliminar.",
+       "href": "./revistas/arrepentios-porque-el-reinio-de-los-cielos-se-ha-acercado/index.html",
+       "cover": "../assets/cover/arrepentios-porque-el-reinio-de-los-cielos-se-ha-acercado.png"
+     },
+     {
+       "title": "Ayúdame a Salir de Aquí",
+       "description": "Versión preliminar.",
+       "href": "./revistas/ayudenme-a-salir-de-aqui/index.html",
+       "cover": "../assets/cover/ayudenme-a-salir-de-aqui.gif"
+     },
+     {
+       "title": "Características Sobresalientes de un Avivamiento",
+       "description": "Versión preliminar.",
+       "href": "./revistas/caracteristicas-sobresalientes-de-un-avivamiento/index.html",
+       "cover": "../assets/cover/caracteristicas-sobresalientes-de-un-avivamiento.png"
+     },
+     {
+       "title": "Carros de Hierro",
+       "description": "Versión preliminar.",
+       "href": "./revistas/carros-de-hierro/index.html",
+       "cover": "../assets/cover/CARROS DE HIERRO.gif"
+     },
+     {
+       "title": "¿Cómo me Arrepiento?",
+       "description": "Versión preliminar.",
+       "href": "./revistas/como-me-arrepiento/index.html",
+       "cover": "../assets/cover/como-me-arrepiento.png"
+     },
+     {
+       "title": "¿Cómo Reconocer si un Mensaje Viene de Dios?",
+       "description": "Versión preliminar.",
+       "href": "./revistas/como-reconocer-si-un-mensaje-viene-de-Dios/index.html",
+       "cover": "../assets/cover/como-reconocer-si-un-mensaje-viene-de-Dios.png"
+     },
+     {
+       "title": "¿Cómo Reconocer un Falso Ministro?",
+       "description": "Versión preliminar.",
+       "href": "./revistas/como-reconocer-un-falso-ministro/index.html",
+       "cover": "../assets/cover/como-reconocer-un-falso-ministro.png"
+     },
+     {
+       "title": "Conducidos a las Tinieblas",
+       "description": "Versión preliminar.",
+       "href": "./revistas/conducidos-a-las-tinieblas/index.html",
+       "cover": "../assets/cover/CONDUCIDOS A LAS TINIEBLAS.gif"
+     },
+     {
+       "title": "¡Cuidado! Un Evangelio Falso está Destruyendo el Cristianismo Bíblico",
+       "description": "Versión preliminar.",
+       "href": "./revistas/cuidado-un-evangelio-falso-esta-destruyendo-el-cristianimo-biblico/index.html",
+       "cover": "../assets/cover/cuidado-un-evangelio-falso-esta-destruyendo-el-cristianimo-biblico.png"
+     },
+     {
+       "title": "El Arnés del Señor",
+       "description": "Versión preliminar.",
+       "href": "./revistas/el-arnes-del-Señor/index.html",
+       "cover": "../assets/cover/el-arnes-del-Señor.png"
+     },
+     {
+       "title": "El Casi Cristiano",
+       "description": "Versión preliminar.",
+       "href": "./revistas/el-casi-cristiano/index.html",
+       "cover": "../assets/cover/EL CASI CRISTIANO.gif"
+     },
+     {
+       "title": "El Día del Juicio Final",
+       "description": "Versión preliminar.",
+       "href": "./revistas/el-dia-del-juicio-final/index.html",
+       "cover": "../assets/cover/el-dia-del-juicio-final.png"
+     },
+     {
+       "title": "El Mensaje Olvidado del Arrepentimiento",
+       "description": "Versión preliminar.",
+       "href": "./revistas/el-mensaje-olvidado-del-arrepentimiento/index.html",
+       "cover": "../assets/cover/el-mensaje-olvidado-del-arrepentimiento.jpg"
+     },
+     {
+       "title": "¿Eres un Verdadero Discípulo de Jesús?",
+       "description": "Versión preliminar.",
+       "href": "./revistas/eres-un-verdadero-discipulo-de-Jesus/index.html",
+       "cover": "../assets/cover/eres-un-verdadero-discipulo-de-Jesus.gif"
+     },
+     {
+       "title": "Estamos Escuchando a un Falso Profeta",
+       "description": "Versión preliminar.",
+       "href": "./revistas/estamos-escuchando-a-un-falso-profeta/index.html",
+       "cover": "../assets/cover/estamos-escuchando-a-un-falso-profeta.png"
+     },
+     {
+       "title": "Gracia Barata",
+       "description": "Versión preliminar.",
+       "href": "./revistas/gracia-barata/index.html",
+       "cover": "../assets/cover/GRACIA BARATA.gif"
+     },
+     {
+       "title": "Isaías 51:1–2",
+       "description": "Versión preliminar.",
+       "href": "./revistas/isaias-51-1-2/index.html",
+       "cover": "../assets/cover/isaias-51-1-2.png"
+     },
+     {
+       "title": "La Apostasía y el Anticristo",
+       "description": "Versión preliminar.",
+       "href": "./revistas/la-apostacia-y-el-anticristo/index.html",
+       "cover": "../assets/cover/la-apostacia-y-el-anticristo.png"
+     },
+     {
+       "title": "La Compañía de Elías",
+       "description": "Versión preliminar.",
+       "href": "./revistas/la-compañia-de-elias/index.html",
+       "cover": "../assets/cover/LA COMPAÑIA DE ELIAS.gif"
+     },
+     {
+       "title": "La Doctrina de Jezabel",
+       "description": "Versión preliminar.",
+       "href": "./revistas/la-doctrina-de-jezabel/index.html",
+       "cover": "../assets/cover/LA DOCTRINA DE JEZABEL.gif"
+     },
+     {
+       "title": "La Oración para el Avivamiento",
+       "description": "Versión preliminar.",
+       "href": "./revistas/la-oracion-para-el-avivamiento/index.html",
+       "cover": "../assets/cover/la-oracion-para-el-avivamiento.png"
+     },
+     {
+       "title": "Obstáculos para el Avivamiento",
+       "description": "Versión preliminar.",
+       "href": "./revistas/obstaculos-para-el-avivamiento/index.html",
+       "cover": "../assets/cover/obstaculos-para-el-avivamiento.png"
+     },
+     {
+       "title": "Ojos Codiciosos",
+       "description": "Versión preliminar.",
+       "href": "./revistas/ojos-codiciosos/index.html",
+       "cover": "../assets/cover/OJOS CODICIOSOS.gif"
+     },
+     {
+       "title": "Pecadores en Manos de un Dios Airado",
+       "description": "Versión preliminar.",
+       "href": "./revistas/pecadores-en-manos-de-un-Dios-airado/index.html",
+       "cover": "../assets/cover/pecadores-en-manos-de-un-Dios-airado.png"
+     },
+     {
+       "title": "Profetas de la Mentira",
+       "description": "Versión preliminar.",
+       "href": "./revistas/profetas-de-la-mentira/index.html",
+       "cover": "../assets/cover/profetas-de-la-mentira.jpg"
+     },
+     {
+       "title": "¿Puede Jesús ser tu Salvador sin ser tu Señor?",
+       "description": "Versión preliminar.",
+       "href": "./revistas/puede-Jesus-ser-tu-salvador-sin-ser-tu-señor/index.html",
+       "cover": "../assets/cover/puede-Jesus-ser-tu-salvador-sin-ser-tu-señor.jpg"
+     },
+     {
+       "title": "Radiografía del Pecado",
+       "description": "Versión preliminar.",
+       "href": "./revistas/radiografia-del-pecado/index.html",
+       "cover": "../assets/cover/radiografia-del-pecado.png"
+     },
+     {
+       "title": "Señor, Danos un Avivamiento",
+       "description": "Versión preliminar.",
+       "href": "./revistas/Señor-danos-un-avivamiento/index.html",
+       "cover": "../assets/cover/Señor-danos-un-avivamiento.jpg"
+     },
+     {
+       "title": "Síntomas que Preceden al Avivamiento",
+       "description": "Versión preliminar.",
+       "href": "./revistas/sintomas-que-presceden-al-avivamiento/index.html",
+       "cover": "../assets/cover/sintomas-que-presceden-al-avivamiento.png"
+     },
+     {
+       "title": "Un Cristianismo Afeminado",
+       "description": "Versión preliminar.",
+       "href": "./revistas/un-cristianismo-afeminado/index.html",
+       "cover": "../assets/cover/UN CRISTIANO AFEMINADO.gif"
+     },
+   {
+     "title": "El Destino del Pecador",
+     "description": "Versión preliminar.",
+     "href": "./revistas/el-destino-del-pecador/index.html",
+     "cover": "../assets/cover/el-destino-del-pecador.gif"
    }
 ];
 
